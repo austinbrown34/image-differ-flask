40d5cb376c24a44a1391018c6426c903a4f8b108
Austin <austinbrown34@gmail.com>
2018-08-12 02:10:59 -0600

    wip

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify, make_response, request, abort
+from flask import Flask, jsonify, make_response, request, abort, Response
 from differ import Differ
+import json
 
 
 app = Flask(__name__)
@@ -11,6 +12,22 @@ EXAMPLE_IMG_B = 'https://s3.amazonaws.com/img-diff/pica.png'
 EXAMPLE_DST = 'compare'
 EXAMPLE_BUCKET = 'img-diff'
 EXAMPLE_BUCKET_PREFIX = 'compare'
+
+
+def build_response(resp_dict, status_code):
+    response = Response(json.dumps(resp_dict), status_code)
+    return response
+
+
+def diff(payload):
+    differ = Differ()
+    differ.diff(
+        payload['before_image_url'],
+        payload['after_image_url'],
+        payload['dst'],
+        payload['bucket'],
+        payload['bucket_prefix']
+    )
 
 
 @app.errorhandler(404)
@@ -27,16 +44,9 @@ def differ():
 def diff():
     if not (request.json):
         abort(400)
-    differ = Differ()
-    response = differ.diff(
-        request.json['before_image_url'],
-        request.json['after_image_url'],
-        request.json['dst'],
-        request.json['bucket'],
-        request.json['bucket_prefix']
-    )
+    diff(request.json)
 
-    return jsonify(response)
+    return build_response({"status": "success"}, 200)
 
 if __name__ == '__main__':
     app.run()
